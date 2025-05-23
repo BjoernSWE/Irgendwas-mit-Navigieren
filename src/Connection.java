b1fb02ca17a08a9d06dc01a9dca06858616e8db9
BjoernSWE <beh6943@thi.de>
2025-05-23 10:10:17 +0200

    Aufgabe 2 fertig

--- src/Connection.java
+++ src/Connection.java
@@ -1,5 +1,8 @@
 public class Connection {
 public final double Distance;
+public City stadt1;
+public City stadt2;
+
 
     public Connection(City stadt1, City stadt2)
     {
@@ -8,23 +11,39 @@ public final double Distance;
 
     double getDistanceInKm(City stadt1, City stadt2) 
     {
-        double lat1 = stadt1.getLatKoord();
-        double lon1 = stadt1.getLongKoord();
-        double lat2 = stadt2.getLatKoord();
-        double lon2 = stadt2.getLongKoord();
-        double earthRadiusKm = 6371;
-        double dLat = degreesToRadians(lat2-lat1);
-        double dLon = degreesToRadians(lon2-lon1);
-        lat1 = degreesToRadians(lat1);
-        lat2 = degreesToRadians(lat2);
+        double lat1 = stadt1.getLatKoord(); //holt sich die Latitude von Stadt 1
+        double lon1 = stadt1.getLongKoord(); //holt sich die Longitude von Stadt 1
+        double lat2 = stadt2.getLatKoord(); //holt sich die Latitude von Stadt 2
+        double lon2 = stadt2.getLongKoord(); //holt sich die Longitude von Stadt 2
+        double earthRadiusKm = 6371; //Radius der Erde in km
+        double dLat = degreesToRadians(lat2-lat1); //Differenz der Breiten
+        double dLon = degreesToRadians(lon2-lon1); //Differenz der Längen
+        lat1 = degreesToRadians(lat1); //Umwandlung der Breite von Stadt 1 in Bogenmaß
+        lat2 = degreesToRadians(lat2); //Umwandlung der Breite von Stadt 2 in Bogenmaß
         double a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) *
-        Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        return earthRadiusKm * c;
+        Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); //Berechnung der Differenz der Breiten und Längen
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); //Berechnung der Differenz der Breiten und Längen
+        return earthRadiusKm * c; //Berechnung der Distanz in km und returnt Diese
         }
 
         private double degreesToRadians(double degrees) 
         {
-            return degrees * Math.PI / 180;
+            return degrees * Math.PI / 180; //Umwandlung der Grad in Bogenmaß
+        }
+
+        public City getOtherCity(City stadt)
+        {
+            if (stadt.getName().equals(stadt1.getName()))
+            {
+                return stadt1;
             }
+            else if (stadt1.getName().equals(stadt2.getName()))
+            {
+                return stadt2;
+            }
+            else
+            {
+                return null;
+            }
+        }
 }
